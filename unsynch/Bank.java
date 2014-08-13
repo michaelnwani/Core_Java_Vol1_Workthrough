70311639a75bc3d1cd27360e734cb9587f76d806
Michael <kmichael24@gmail.com>
2014-08-13 00:03:02 -0400

    Added Locks

--- unsynch/Bank.java
+++ unsynch/Bank.java
@@ -1,5 +1,5 @@
 package unsynch;
-
+import java.util.concurrent.locks.*;
 /**
  * A bank with a number of bank accounts.
  * @author michaelnwani
@@ -8,6 +8,7 @@ package unsynch;
 public class Bank 
 {
 	private final double[] accounts;
+	private Lock bankLock = new ReentrantLock();
 	
 	/**
 	 * Constructs the bank.
@@ -31,12 +32,20 @@ public class Bank
 	 */
 	public void transfer(int from, int to, double amount)
 	{
-		if (accounts[from] < amount) return;
-		System.out.print(Thread.currentThread());
-		accounts[from] -= amount;
-		System.out.printf("%10.2f from %d to %d", amount, from, to);
-		accounts[to] += amount;
-		System.out.printf("Total Balance: %10.2f%n", getTotalBalance());
+		bankLock.lock();
+		try
+		{
+			if (accounts[from] < amount) return;
+			System.out.print(Thread.currentThread());
+			accounts[from] -= amount;
+			System.out.printf("%10.2f from %d to %d", amount, from, to);
+			accounts[to] += amount;
+			System.out.printf("Total Balance: %10.2f%n", getTotalBalance());
+		}
+		finally
+		{
+			bankLock.unlock();
+		}
 	}
 	
 	/**
